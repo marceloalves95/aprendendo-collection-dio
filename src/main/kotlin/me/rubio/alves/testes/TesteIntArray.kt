0a1a4bf624f0d87b562d925a46a9c76906a4a0a9
Marcelo Alves <marceloalves1295@gmail.com>
2021-02-19 13:40:44 -0300

    Primeiro commit

---
+++ src/main/kotlin/me/rubio/alves/testes/TesteIntArray.kt
@@ -0,0 +1,46 @@
+package me.rubio.alves.testes
+
+/*
+ *@created 19/02/2021 - 13:26
+ *@project aprendendo-collection-dio
+ *@author Rubio Alves
+*/
+fun main() {
+
+    val values = IntArray(5)
+
+    values[0] = 1
+    values[1] = 7
+    values[2] = 6
+    values[3] = 3
+    values[4] = 2
+
+    for(valor in values){
+
+        println(valor)
+
+    }
+    println("------------------------------")
+    values.forEach {valor ->
+
+        println(valor)
+
+    }
+    println("------------------------------")
+    for (index in values.indices){
+
+        println(values[index])
+
+
+    }
+    println("------------------------------")
+    values.sort()
+    for (valor in values){
+
+        println(valor)
+
+
+    }
+
+
+}
