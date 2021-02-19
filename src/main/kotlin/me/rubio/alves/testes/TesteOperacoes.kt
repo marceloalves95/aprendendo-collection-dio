f98573fd33116fe5c518e6c10c87663c0963e9b3
Marcelo Alves <marceloalves1295@gmail.com>
2021-02-19 15:31:34 -0300

    Alteração do projeto

--- src/main/kotlin/me/rubio/alves/testes/TesteOperacoes.kt
+++ src/main/kotlin/me/rubio/alves/testes/TesteOperacoes.kt
@@ -15,12 +15,25 @@ fun main() {
 
     }
     println("----------------------------")
-    println("Maior Salário: ${salarios.max()}")
-    println("Menor Salário: ${salarios.min()}")
+    println("Maior Salário: ${salarios.maxOrNull()}")
+    println("Menor Salário: ${salarios.minOrNull()}")
     println("Média Salárial: ${salarios.average()}")
 
     val salariosMaiorque2500 = salarios.filter { it > 2500 }
     println("----------------------------")
     salariosMaiorque2500.forEach{ println(it)}
+
+    println("----------------------------")
+    println(salarios.count { it in 2000.0..5000.0 })
+
+    println("----------------------------")
+    println(salarios.find { it == 2250.0 })
+    println(salarios.find { it == 500.0 })
+
+    println("----------------------------")
+    println(salarios.any { it == 1000.0 })
+    println(salarios.any { it == 500.0 })
+
+
 
 }
